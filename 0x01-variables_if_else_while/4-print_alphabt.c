7c89d403f43668bcd1a28a2609ea43a5a227ddc6
Ifiok Ekott <ekottifiok@gmail.com>
2022-06-03 22:45:12 +0100

    alphabet soup

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/* more headers goes there */
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+/* betty style doc for function main goes there */
+
+int main(void)
+{
+	int i;
+
+	for (i = 97; i < 123; i++)
+		if (i != 101 || i != 113)
+			putchar(i);
+
+	putchar(10);
+	return (0);
+}

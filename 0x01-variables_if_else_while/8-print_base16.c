66c4441a09f3af8e7e286994fa52987acf0b435b
Ifiok Ekott <ekottifiok@gmail.com>
2022-06-04 00:02:57 +0100

    Hexadecimal

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+/* more headers goes there */
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+/* betty style doc for function main goes there */
+
+int main(void)
+{
+	int i;
+
+	for (i = 48; i < 58; i++)
+		putchar(i);
+    
+    for (i=97; i < 103; i++)
+        putchar(i);
+
+	putchar(10);
+	return (0);
+}

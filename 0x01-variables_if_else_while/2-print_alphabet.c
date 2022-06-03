feacc25c3e5c5dcc51ab377f350052c0b23bb002
Ifiok Ekott <ekottifiok@gmail.com>
2022-06-03 21:55:01 +0100

    I sometimes suffer from insomnia

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/* more headers goes there */
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+/* betty style doc for function main goes there */
+
+int main(void)
+{
+	int i;
+	char *alpha;
+
+	for (i = 97; i < 123; i++)
+		putchar(i);
+
+	putchar(10);
+	return (0);
+}

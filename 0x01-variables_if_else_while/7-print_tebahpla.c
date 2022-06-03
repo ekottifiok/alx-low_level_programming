9638a09aa893881c337508786ac4f88c3e1e125d
Ifiok Ekott <ekottifiok@gmail.com>
2022-06-03 23:57:06 +0100

    Smile in the mirror

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/* more headers goes there */
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+/* betty style doc for function main goes there */
+
+int main(void)
+{
+	int i;
+
+	for (i = 122; i > 96; i--)
+		putchar(i);
+
+	putchar(10);
+	return (0);
+}

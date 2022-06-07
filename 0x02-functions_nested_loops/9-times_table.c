92f5aed32ddff66ebdfcbfbe6a1e970cada4251f
Ifiok Ekott <ekottifiok@gmail.com>
2022-06-07 23:45:32 +0100

    Learn your times table 4

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -13,13 +13,14 @@ void times_table(void)
 {
 	int a, i, j;
 
-	for (i = 0; i < 9; i++)
+	for (i = 0; i < 10; i++)
 	{
-		for (j = 0; j < 9; j++)
+		for (j = 0; j < 10; j++)
 		{
 			a = i * j;
 			if (a > 9)
 				_putchar((a / 10) + '0');
+			_putchar(44);
 			_putchar((a % 10) + '0');
 			_putchar(44);
 			_putchar(32);

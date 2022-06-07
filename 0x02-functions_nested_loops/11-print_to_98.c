b65803fd2feb097b44d57297f307b27b0779a0f4
Ifiok Ekott <ekottifiok@gmail.com>
2022-06-08 00:33:40 +0100

    98 Battery Street, the OG

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,27 @@
+#include "main.h"
+#include <stdio.h>
+
+/* more headers goes there */
+/**
+ * add - Prints a to z
+ * @i: integer one
+ * @j: integer two
+ * Return: the sum of two values
+ */
+
+void print_to_98(int n);
+{
+	int i;
+
+	for (i = 0; i < 99; i++)
+	{
+		if (i == 0)
+		{
+			_putchar('0');
+		}
+		_putchar(44);
+		_putchar(32);
+		_putchar(i + '0')
+	}
+	
+}

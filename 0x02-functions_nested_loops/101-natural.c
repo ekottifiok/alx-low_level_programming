42c1f59bba8784256674edc9380cb261ab570d39
Ifiok Ekott <ekottifiok@gmail.com>
2022-06-08 01:59:55 +0100

    Nature made the natural numbers; All else is the 2

--- 0x02-functions_nested_loops/101-natural.c
+++ 0x02-functions_nested_loops/101-natural.c
@@ -8,12 +8,12 @@
  * Returns: Nil
  */
 
-void main(void)
+int main(void)
 {
-	int i;
+	int i, sum = 0;
 
 	for (i = 0; i < 1024; i++)
 		if ((i % 3 == 0) && (i % 5 == 0))
-			printf("%d", i);
-
+			sum += i;
+	printf("%d", sum);
 }

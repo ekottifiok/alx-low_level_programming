373a38593a7e47909e5666f1420002ccff698318
Ifiok Ekott <ekottifiok@gmail.com>
2022-06-08 01:34:14 +0100

    98 Battery Street, the OG 9

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -4,7 +4,7 @@
 /* more headers goes there */
 /**
  * print_to_98 - Prints a to z
- * @i: integer one
+ *
  * @n: integer two
  * Return: the sum of two values
  */
@@ -12,24 +12,18 @@
 void print_to_98(int n)
 {
 	int i;
+
 	if (n < 98)
 	{
 		for (i = 0; n < 99; n++, i++)
 		{
 			if (i == 0)
 			{
-				if (n > 9)
-				{
-					_putchar((n / 10) + '0');
-				}
-				_putchar((n % 10) + '0');
+				printf("%d", n);
 			}
 			else
-			{	_putchar(44);
-				_putchar(32);
-				if (n > 9)
-					_putchar((n / 10) + '0');
-				_putchar((n % 10) + '0');
+			{
+				printf(", %d", n);
 			}
 		}
 	}
@@ -40,31 +34,11 @@ void print_to_98(int n)
 
 			if (i == 0)
 			{
-				if (n > 99)
-				{
-					_putchar((n / 100) + '0');
-					_putchar(((n / 10) % 10) + '0');
-				}
-				else if (n > 9)
-				{
-					_putchar((n / 10) + '0');
-				}
-				_putchar((n % 10) + '0');
+				printf("%d", n);
 			}
 			else
 			{
-				_putchar(44);
-				_putchar(32);
-				if (n > 99)
-				{
-					_putchar((n / 100) + '0');
-					_putchar(((n / 10) % 10) + '0');
-				}
-				else if (n > 9)
-				{
-					_putchar((n / 10) + '0');
-				}
-				_putchar((n % 10) + '0');
+				printf(", %d", n);
 			}
 		}
 	}

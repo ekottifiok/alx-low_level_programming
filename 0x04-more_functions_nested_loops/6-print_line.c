cf27524c3a4606f8f5c1322c6905d0bf867a3dcc
Ifiok Ekott <ekottifiok@gmail.com>
2022-06-09 22:36:55 +0100

    The shortest distance between two points is a

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * print_line - tests function
+ * @n: value inputted
+ * Return: 1 if uppercase and 0 otherwise
+ */
+
+void print_line(int n)
+{
+	int i = 0;
+
+	if (n <= 0)
+	{
+		_putchar('\n');
+	}
+
+	while (i != n)
+	{
+		_putchar('_');
+		i++;
+	}
+}

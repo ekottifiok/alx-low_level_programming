c259397d1b7a7ce3d0fe6f960c5c5d2977f87217
Ifiok AK-PC <ekottifiok@gmail.com>
2022-06-10 00:21:02 +0100

    another pc

---
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -0,0 +1,21 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * main - entry to function
+ * Return: 1 if uppercase and 0 otherwise
+ */
+
+void print_triangle(void)
+{
+	int size = 3;
+	int i = 0, j;
+
+	while (i != size)
+	{
+		while (i != size)		
+		{
+			_putchar('#');
+		}
+	}
+}

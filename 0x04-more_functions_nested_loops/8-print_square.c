8dab54ac3a033239c9db6d486153a74a41a87e53
Ifiok Ekott <ekottifiok@gmail.com>
2022-06-09 23:01:55 +0100

    You are so much sunshine in every square inch

---
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * print_square - tests function
+ * @size: value inputted
+ * Return: 1 if uppercase and 0 otherwise
+ */
+
+void print_square(int size)
+{
+	int i = 0;
+
+	while (i != size)
+	{
+		while (i != size)
+		{
+			_putchar('#');
+		}
+		}
+	_putchar(10);
+}

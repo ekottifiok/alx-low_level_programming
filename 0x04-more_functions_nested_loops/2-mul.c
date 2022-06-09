0b40ed7025ad1833aba12392b326d8d66cdddd8e
Ifiok Ekott <ekottifiok@gmail.com>
2022-06-09 20:48:59 +0100

    Collaboration is multiplication

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * mul - tests function that prints if integer is positive or negative
+ * @a: first value
+ * @b: second value
+ * Return: 1 if uppercase and 0 otherwise
+ */
+
+int mul(int a, int b)
+{
+	return (a * b);
+}

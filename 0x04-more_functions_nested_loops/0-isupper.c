c52947fa8933543f09ca52d8e388efa4d9c98345
Ifiok Ekott <ekottifiok@gmail.com>
2022-06-09 19:46:36 +0100

    isupper 2

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -1,11 +1,18 @@
 #include "main.h"
 
+/**
+ * _isupper - tests function that prints if integer is positive or negative
+ * @c: int inputed
+ * Return: 1 if uppercase and 0 otherwise
+ */
+
 int _isupper(int c)
 {
-    char d = c;
-    if (d > 'A' || d < 'Z')
-    {
-        return 1;
-    }
-    return 0;
+	char d = c;
+
+	if (d >= 'A' || d <= 'Z')
+	{
+		return (1);
+	}
+	return (0);
 }

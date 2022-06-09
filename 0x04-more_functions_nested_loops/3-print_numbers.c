c5a49055641ff41877aa7402d7b99b919c3aaf0e
Ifiok Ekott <ekottifiok@gmail.com>
2022-06-09 21:03:13 +0100

    The numbers speak for themselves

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * print_numbers - tests function
+ * Return: 1 if uppercase and 0 otherwise
+ */
+
+void print_numbers(void)
+{
+	for (int i = 48; i <= 57; i++)
+	{
+		_putchar(i);
+	}
+	_putchar(10);
+}

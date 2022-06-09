a0ebbcaff5e9d6fface09f90ad718634e0c99043
Ifiok Ekott <ekottifiok@gmail.com>
2022-06-09 22:52:23 +0100

    I feel like I am diagonally parked in a parallel

---
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * print_diagonal - tests function
+ * @n: value inputted
+ * Return: 1 if uppercase and 0 otherwise
+ */
+
+void print_diagonal(int n)
+{
+	int i = 0;
+
+	if (n <= 0)
+	{
+		_putchar('\n');
+		return;
+	}
+
+	while (i != n)
+	{
+		_putchar('\\');
+		i++;
+	}
+	_putchar(10);
+}

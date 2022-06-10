e60eb503b38195209eb94a6282d3d69d43ea076b
Ifiok Ekott <ekottifiok@gmail.com>
2022-06-10 03:28:17 +0100

    scam 20

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -1,22 +1,35 @@
 #include "main.h"
 
 /**
- * print_number - prints an integer
- * @n: tracked var
+ * print_number - prints an integer.
+ * @n: input integer.
+ * Return: no return.
  */
-
 void print_number(int n)
 {
-    int x = n;
+    unsigned int m, d, count;
 
     if (n < 0)
     {
-        _putchar('-');
-        x = -x;
+        _putchar(45);
+        m = n * -1;
     }
-    if ((x / 10) > 0)
+    else
     {
-        print_number(x / 10);
+        m = n;
     }
-    _putchar((x % 10) + 48);
+
+    d = m;
+    count = 1;
+
+    while (d > 9)
+    {
+        d /= 10;
+        count *= 10;
+    }
+
+    for (; count >= 1; count /= 10)
+    {
+        _putchar(((m / count) % 10) + 48);
+    }
 }

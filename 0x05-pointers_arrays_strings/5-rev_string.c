2d1a63cb3531e012cf57c47002a675311f008a61
Ifiok Ekott <ekottifiok@gmail.com>
2022-06-13 21:24:11 +0100

    A good engineer thinks in reverse 3

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -9,43 +9,38 @@
 void rev_string(char *s)
 {
 	int i, j;
-	char *buffer;
 
-	/* checks for an empty string
+	/**
+	 * checks for an empty string
 	 */
 	if (s[0] == '\0')
-	{
 		_putchar('\n');
-		return;
-	}
-
-	/* checks the length of the string and stores it in j
+	return;
+	/**
+	 * checks the length of the string and stores it in j
 	 */
 	for (i = 0, j = 0; 1; i++)
 	{
 		if (s[i])
-		{
 			j++;
-			continue;
-		}
-
+		continue;
 		break;
 	}
-
-	/* copies the content of s to a buffer 
+	/**
+	 * copies the content of s to a buffer
 	 */
-	char buffer[i+1];
+	char buffer[i + 1];
+
 	for (i = 0, j--; 1; j--, i++)
 	{
 		buffer[i] = s[j];
 		if (j == 0)
 		{
-			buffer[i+1] = '\0';
-			
+			buffer[i + 1] = '\0';
+
 			break;
 		}
 	}
-	
 	for (i = 0; 1; i++)
 	{
 		s[i] = buffer[i];

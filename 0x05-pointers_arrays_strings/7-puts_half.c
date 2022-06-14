924f9bf8155866b1515455247ae43fd88708fc05
Ifiok Ekott <ekottifiok@gmail.com>
2022-06-14 02:53:16 +0100

    Winning is only half of it. Having fun is the other half

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+/**
+ * rev_string - prints a string, in reverse, followed by a new line..
+ * @s: input integer.
+ * Return: no return.
+ */
+
+void puts_half(char *str)
+{
+    int i;
+    char str[] = "0123456789";
+    /**
+	 * checks the length of the string and stores it in j
+	 */
+	while (str[i] != '\0')
+	{
+		i++;
+	}
+
+    if 
+
+}

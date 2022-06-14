bfb986df85f159ea56050af07ff1dd8c0e3fa8bc
Ifiok Ekott <ekottifiok@gmail.com>
2022-06-14 02:18:51 +0100

    Half the lies they tell about me aren't true 2

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -11,13 +11,19 @@ void puts2(char *str)
 {
 	int i;
 
-	for (i = 0; 1; i++)
+	while (str[i] = '\0')
+	{
+		i++;
+	}
+	
+	while(i != 0)
 	{
 
 		if (i % 2 == 0)
 		{
 			_putchar(str[i]);
 		}
+		i--;
 	}
 	_putchar(10);
 }

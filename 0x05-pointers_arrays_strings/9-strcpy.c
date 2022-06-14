4d1e1285a639fb04e9c0d9ae18432cd0eaafb9ce
Ifiok Ekott <ekottifiok@gmail.com>
2022-06-14 03:59:21 +0100

    Harcode Sting copy

--- 0x05-pointers_arrays_strings/9-strcpy.c
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -2,27 +2,28 @@
 #include <stdio.h>
 
 /**
- * print_array - prints a string, in reverse, followed by a new line..
- * @a: input integer.
- * @n: input integer
+ * _strcpy - prints a string, in reverse, followed by a new line..
+ * @dest: input integer.
+ * @src: input integer
  * Return: no return.
  */
 
 char *_strcpy(char *dest, char *src)
 {
-    if (dest == NULL) {
-        return NULL;
-    }
+	if (dest == NULL)
+	{
+		return (NULL);
+	}
 
-    char *ptr = dest;
-    while (*src != '\0')
-    {
-        *dest = *src;
-        dest++;
-        src++;
-    }
+	char *ptr = dest;
 
-    *dest = '\0';
-    return ptr
+	while (*src != '\0')
+	{
+		*dest = *src;
+		dest++;
+		src++;
+	}
 
+	*dest = '\0';
+	return (ptr);
 }

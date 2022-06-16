6b915a1ba00a685f2da80a3794f848b7a3bb75bd
Ifiok Ekott <ekottifiok@gmail.com>
2022-06-16 21:36:11 +0100

    Paranoid of reverse 3

--- 0x06-pointers_arrays_strings/4-rev_array.c
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -6,17 +6,16 @@
  * @n: input integer
  * Return: no return.
  */
-
 void reverse_array(int *a, int n)
 {
 
-	int buffer, i, j;
+	int buffer, i;
 
-	for (i = 0, n--; i < n / 2; i++, n--)
+	for (i = 0, n--; i <= n / 2; i++)
 	{
 
-		buffer = a[n];
-		a[n] = a[i];
+		buffer = a[n - i];
+		a[n - i] = a[i];
 		a[i] = buffer;
 	}
 }

1e580e7eb6c0da49b9c12f91e418b39307ac36f3
Ifiok Ekott <ekottifiok@gmail.com>
2022-06-16 16:17:56 +0100

    strcmp 1

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -9,5 +9,17 @@
 
 int _strcmp(char *s1, char *s2)
 {
-    
+	while (*s1 == *s2)
+	{
+		if (*s1 == *s2)
+		{
+			return (0);
+		}
+		s1++;
+		s2++;
+		
+	}
+	printf("%c", *s1);
+	return (*s1 - *s2);
+	
 }

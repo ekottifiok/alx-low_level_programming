ee737aa08d8e525d94bad4beae2798a12b81fa6a
Ifiok Ekott <ekottifiok@gmail.com>
2022-06-17 05:05:58 +0100

    Numbers have life

--- 0x06-pointers_arrays_strings/100-rot13.c
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -12,6 +12,7 @@ char *rot13(char *n)
 	char lookout[] = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
 	char replacement[] = "NOPQRSTUVWXYZABCDEFGHIJKLMnopqrstuvwxyzabcdefghijklm";
 	int i, j;
+	
 	for (i = 0; n[i] != '\0'; i++)
 	{
 		for (j = 0; lookout[j] != '\0'; j++)

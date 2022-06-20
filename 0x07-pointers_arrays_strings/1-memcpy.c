30ae82ced3f2c6f199bdbbdec09e2bec5c7c8999
Ifiok Ekott <ekottifiok@gmail.com>
2022-06-20 18:49:16 +0100

    memcpy

--- 0x07-pointers_arrays_strings/1-memcpy.c
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -11,11 +11,11 @@
 
 char *_memcpy(char *dest, char *src, unsigned int n)
 {
-	int i;
+	unsigned int i;
 
-	for (i = 0; i != n; i++, src++, dest++)
+	for (i = 0; i < n; i++)
 	{
-		*dest = *src;
+		dest[i] = src[i];
 	}
 
 	return (src);

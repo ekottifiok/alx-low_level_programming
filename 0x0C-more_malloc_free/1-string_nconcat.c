1af1f688247bedb134de320aa9b4559ec9efcb93
Ifiok Ekott <ekottifiok@gmail.com>
2022-07-01 05:57:26 +0100

    made

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -1,60 +1,49 @@
 #include "main.h"
 
 /**
- * string_nconcat -  
- * 
+ * string_nconcat - joins two string
+ *
  * @s1: first part of the string
  * @s2: second part of the string
  * @n: number of times the string should run
- * Return: char* 
+ * Return: char*
  */
-
 
 char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
-    char *ptr;
-    unsigned int length_s1, length_s2, total, i, j;
-    
-    if (!(s1) && !(s2))
-        return (NULL);
+	char *ptr;
+	unsigned int length_s1, length_s2, total, i, j;
 
-    for (length_s1 = 0; s1[length_s1]; length_s1++);
-    for (length_s2 = 0; s2[length_s2]; length_s2++);
+	if (!(s1) && !(s2))
+		return (NULL);
 
-    if (n >= length_s2)
-        return (s2);
+	for (length_s1 = 0; s1[length_s1]; length_s1++)
+		;
+	for (length_s2 = 0; s2[length_s2]; length_s2++)
+		;
 
-    
-    total = length_s1 + length_s2;
-    if (n < length_s2)
-    {
-        ptr = (char *)malloc(sizeof(char) * (length_s1 + n + 1));
-        total = length_s1 + n + 1;
-    }
-    else
-    { 
-        ptr = (char *)malloc(sizeof(char) * (total + 1));
-    }
-    if (ptr == NULL)
-        return (NULL);
+	if (n >= length_s2)
+		return (s2);
 
-    for (i = 0; *s1; i++, s1++)
-        ptr[i] = *s1;
+	total = length_s1 + length_s2;
+	if (n < length_s2)
+	{
+		ptr = (char *)malloc(sizeof(char) * (length_s1 + n + 1));
+		total = length_s1 + n + 1;
+	}
+	else
+	{
+		ptr = (char *)malloc(sizeof(char) * (total + 1));
+	}
+	if (ptr == NULL)
+		return (NULL);
 
-    
-    for (; *s2 && n > 0; i++, s2++, n--)
-        ptr[i] = *s2;
-    
-    ptr[i] = '\0';
-    return(ptr);
-}
+	for (i = 0; *s1; i++, s1++)
+		ptr[i] = *s1;
 
-int main(void)
-{
-    char *concat;
+	for (; *s2 && n > 0; i++, s2++, n--)
+		ptr[i] = *s2;
 
-    concat = string_nconcat("Best ", "School !!!", 6);
-    printf("%s\n", concat);
-    free(concat);
-    return (0);
+	ptr[i] = '\0';
+	return (ptr);
 }

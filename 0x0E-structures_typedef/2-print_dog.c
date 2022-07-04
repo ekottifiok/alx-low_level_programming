7560c04d363b638dff9052886a083fc5057cee23
Ifiok Ekott <ekottifiok@gmail.com>
2022-07-04 17:44:35 +0100

    added a check to 5-free_dog.c

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -1,7 +1,7 @@
 #include "dog.h"
 #include <stdio.h>
 #ifndef NULL_CHECKER
-#define NULL_CHECKER(val) ((val==NULL) ? ("nil"): (val))
+#define NULL_CHECKER(val) ((val == NULL) ? ("nil") : (val))
 #endif
 
 /**

9438885bb01586a325d74e6253b83fc8d1681029
Ifiok Ekott <ekottifiok@gmail.com>
2022-07-07 05:50:45 +0100

    100 Scam

--- 0x0F-function_pointers/100-main_opcodes.c
+++ 0x0F-function_pointers/100-main_opcodes.c
@@ -2,41 +2,41 @@
 #include <stdlib.h>
 
 /**
- * error - returns errors
+ * main - prints its own opcodes
+ * @argc: number of arguments
+ * @argv: array of arguments
  *
- * @status: status code of exit
+ * Return: Always 0 (Success)
  */
-void error(int status)
+int main(int argc, char *argv[])
 {
-	printf("Error");
-	exit(status);
-}
-
-/**
- * main - function entrypoint
- *
- * @argc: number of arguments passed
- * @argv: arguments passed in array
- * Return: int
- */
-int main(int argc, char const *argv[])
-{
-	char *array_opcode;
-	int i, value;
+	int bytes, i;
+	char *arr;
 
 	if (argc != 2)
-		error(1);
-
-	value = atoi(argv[1]);
-	if (value < 0)
-		error(2);
-
-	array_opcode = (char *)main;
-	for (i = 0; i < value; i++)
 	{
-		printf("%02hhx ", array_opcode[i]);
+		printf("Error\n");
+		exit(1);
 	}
-	printf("\n");
 
+	bytes = atoi(argv[1]);
+
+	if (bytes < 0)
+	{
+		printf("Error\n");
+		exit(2);
+	}
+
+	arr = (char *)main;
+
+	for (i = 0; i < bytes; i++)
+	{
+		if (i == bytes - 1)
+		{
+			printf("%02hhx\n", arr[i]);
+			break;
+		}
+		printf("%02hhx ", arr[i]);
+	}
 	return (0);
 }

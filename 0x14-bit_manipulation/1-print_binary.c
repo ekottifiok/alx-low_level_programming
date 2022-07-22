e4d289ac21641f2937a826b6cce406ae88477dff
Ifiok Ekott <ekottifiok@gmail.com>
2022-07-22 05:37:30 +0100

    made some changes

--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -1,18 +1,6 @@
 #include "main.h"
 
 /**
- * _putchar - writes the character c to stdout
- * @c: The character to print
- *
- * Return: On success 1.
- * On error, -1 is returned, and errno is set appropriately.
- */
-int _putchar(char c)
-{
-	return (write(1, &c, 1));
-}
-
-/**
  * print_binary - prints the binary equivalent of a decimal number
  * @n: number to print in binary
  */

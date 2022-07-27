6b5556a79d866201d1589e5fe0d91e55822b9948
Ifiok Ekott <ekottifiok@gmail.com>
2022-07-27 07:38:53 +0100

    fixed betty

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -13,7 +13,7 @@ int create_file(const char *filename, char *text_content)
 
 	if (!text_content)
 		return (1);
-		
+
 	descriptor_int = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
 	if (!filename || descriptor_int < 0)
 	{

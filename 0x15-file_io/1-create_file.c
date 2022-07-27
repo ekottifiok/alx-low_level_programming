730d702ad11b107d1d4ad9dd09d60c71dd6c2905
Ifiok Ekott <ekottifiok@gmail.com>
2022-07-27 07:37:33 +0100

    fixed 1-create_file.c

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -11,8 +11,11 @@ int create_file(const char *filename, char *text_content)
 {
 	int descriptor_int, write_int, text_number;
 
+	if (!text_content)
+		return (1);
+		
 	descriptor_int = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
-	if (!filename || descriptor_int < 0 || !text_content)
+	if (!filename || descriptor_int < 0)
 	{
 		close(descriptor_int);
 		return (-1);

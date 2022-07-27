a2a7083b22f60d61516db234cf19423be164a8e7
Ifiok Ekott <ekottifiok@gmail.com>
2022-07-27 05:52:36 +0100

    add a return 1 to append text

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -13,6 +13,8 @@ int append_text_to_file(const char *filename, char *text_content)
 
 	if (!filename)
 		return (-1);
+	if (!text_content)
+		return (1);
 	descriptor_int = open(filename, O_WRONLY | O_APPEND);
 	if (descriptor_int < 0)
 	{

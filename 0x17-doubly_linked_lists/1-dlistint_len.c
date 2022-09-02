f4ec9d17b7fa1ed8ae3176c2970732056b6a1f0d
Ifiok Ekott <ekottifiok@gmail.com>
2022-09-02 07:40:24 +0100

    corrected some errors

--- 0x17-doubly_linked_lists/1-dlistint_len.c
+++ 0x17-doubly_linked_lists/1-dlistint_len.c
@@ -1,18 +1,21 @@
 #include "lists.h"
 
 /**
- * dlistint_len - finds the length of the list 
- * 
+ * dlistint_len - finds the length of the list
+ *
  * @h: the len of list int to be found
  * Return: size_t of the list
  */
-size_t dlistint_len(const dlistint_t *h) 
+size_t dlistint_len(const dlistint_t *h)
 {
-    size_t iter;
-    while (h->prev)
-        h = h->prev;
+	size_t iter;
 
-    for (iter = 0; h != NULL; iter++, h = h->next)
-        ;
-    return (iter);
+	if (!h)
+		return (0);
+	while (h->prev)
+		h = h->prev;
+
+	for (iter = 0; h != NULL; iter++, h = h->next)
+		;
+	return (iter);
 }

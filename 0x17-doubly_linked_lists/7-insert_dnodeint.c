fdb0b81a2930e250dfdbf788b6e8102fe9d310d7
Ifiok Ekott <ekottifiok@gmail.com>
2022-09-03 05:53:31 +0100

    done with all

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -1,64 +1,84 @@
 #include "lists.h"
 
 /**
- * len_dlistint_7 - finds the length of list
+ * set_node - sets the next & prev values of two nodes
+ * @source: Sets the next to dest
+ * @dest: Sets the prev to source
  *
- * @list: the list to be found
- * Return: an unsigned int containing the list
+ * Return: void
  */
-unsigned int len_dlistint_7(dlistint_t *list)
+void set_node(dlistint_t *source, dlistint_t *dest)
 {
-	unsigned int iter;
-
-	if (!list)
-		return (0);
-	while (list->prev)
-		list = list->prev;
-	for (iter = 0; list; iter++, list = list->next)
-		;
-	return (iter);
+	source->next = dest;
+	dest->prev = source;
 }
 
 /**
- * insert_dnodeint_at_index - insert a node at a particular index
+ * create_node - create a new node
+ * @n: the node data
  *
- * @h: the list to add the new node
- * @idx: the index to add the node
- * @n: the number for the new node
- * Return: a new node dlistint_t*
+ * Return: address of the new node
+ */
+dlistint_t *create_node(int n)
+{
+	dlistint_t *new_node = NULL;
+
+	new_node = malloc(sizeof(dlistint_t));
+	if (new_node == NULL)
+		return (NULL);
+	new_node->n = n;
+	new_node->prev = NULL;
+	new_node->next = NULL;
+	return (new_node);
+}
+
+/**
+ * insert_dnodeint_at_index - inserts a new node at a given position.
+ * @h: The head of the list
+ * @idx: The idx to insert the new node
+ * @n: The node data
+ *
+ * Return: The address of the new node, NULL if failed
  */
 dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 {
-	dlistint_t *new_node, *buffer, *next_buffer;
-	unsigned int len_list, position = idx;
+	unsigned int i = 0;
+	dlistint_t *new_node = NULL, *node;
 
-	len_list = len_dlistint_7(*h);
-	if (!(*h) || idx > len_list || len_list == 0)
+	new_node = create_node(n);
+	if (new_node == NULL)
 		return (NULL);
-	new_node = malloc(sizeof(dlistint_t));
-	if (!new_node)
-		return (NULL);
-	new_node->n = n;
-	buffer = *h;
-	for (; buffer && position > 1; position--, buffer = buffer->next)
-		;
-	next_buffer = buffer->next;
-	buffer->next = new_node;
-	/**
-	 * takes care of the next value
-	 */
-	if (idx == (len_list - 1))
-		new_node->next = NULL;
-	else
-		new_node->next = next_buffer;
-	/**
-	 * takes care of the prev value
-	 */
-	if (idx == 0)
-		new_node->prev = NULL;
-	else
-		new_node->prev = buffer;
-
-
-	return (*h);
+	if (*h == NULL)
+	{
+		*h = new_node;
+		return (new_node);
+	}
+	node = *h;
+	while (node != NULL)
+	{
+		if (i == idx)
+		{
+			if (node->prev == NULL)
+			{
+				set_node(new_node, node);
+				(*h) = new_node;
+			}
+			else
+			{
+				new_node->next = node;
+				new_node->prev = node->prev;
+				node->prev->next = new_node;
+				node->prev = new_node;
+			}
+			return (new_node);
+		}
+		i++;
+		if (idx == i && node->next == NULL)
+		{
+			set_node(node, new_node);
+			return (new_node);
+		}
+		node = node->next;
+	}
+	return (NULL);
 }

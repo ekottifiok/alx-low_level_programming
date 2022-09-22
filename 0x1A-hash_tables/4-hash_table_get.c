bdf4a17cecf355adc7c8f255faf6f1799160845e
Ifiok Ekott <ekottifiok@gmail.com>
2022-09-22 23:07:54 +0100

    started working on 4-hash_table_get.c memory leaks occur but not my fault my code gets the value from the hash table

---
+++ 0x1A-hash_tables/4-hash_table_get.c
@@ -0,0 +1,22 @@
+#include "hash_tables.h"
+
+/**
+ * hash_table_get - retrieves a value associated with a key.
+ * @ht: the hash table where the data will be added
+ * @key: the key of the value
+ * Return: the value stored in the list
+ */
+char *hash_table_get(const hash_table_t *ht, const char *key)
+{
+	unsigned long int index;
+	hash_node_t *data_found;
+
+	if (key == NULL || *key == '\0')
+		return (NULL);
+
+	index = key_index((const unsigned char *) key, ht->size);
+	data_found = ht->array[index];
+	if (data_found == NULL)
+		return (NULL);
+	return (data_found->value);
+}

a5831fdbbe67e25b1bb2d83cc2c3328b95795b39
Ifiok Ekott <ekottifiok@gmail.com>
2022-12-26 13:57:36 +0100

    removed the main block that was used to test 1-strncat.c

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -14,19 +14,11 @@ char *_strncat(char *dest, char *src, int n)
 
 	/* counter for dest */
 	for (i = 0; dest[i] != '\0'; i++)
-	;
+		;
 
 	for (j = 0; src[j] != '\0' && j < n; i++, j++)
 		dest[i] = src[j];
 
 	dest[i] = '\0';
 	return (dest);
-}
-
-int main(int argc, char const *argv[])
-{
-	char s1[98] = "Holberton ", s2[] = "School!\n";
-	char *test = _strncat(s1, s2, 5);
-	puts(test);
-	return 0;
 }

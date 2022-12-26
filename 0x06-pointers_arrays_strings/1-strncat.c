801f9d8aee1885b0425b5f787835b7e40a2abce5
Ifiok Ekott <ekottifiok@gmail.com>
2022-12-26 13:55:21 +0100

    fixed 1-strncat.c

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -12,16 +12,21 @@ char *_strncat(char *dest, char *src, int n)
 {
 	int i, j;
 
+	/* counter for dest */
 	for (i = 0; dest[i] != '\0'; i++)
-	{
-		continue;
-	}
+	;
 
-	for (j = 0; src[j] == '\0' && j < n; i++, j++)
-	{
+	for (j = 0; src[j] != '\0' && j < n; i++, j++)
 		dest[i] = src[j];
-	}
 
 	dest[i] = '\0';
 	return (dest);
+}
+
+int main(int argc, char const *argv[])
+{
+	char s1[98] = "Holberton ", s2[] = "School!\n";
+	char *test = _strncat(s1, s2, 5);
+	puts(test);
+	return 0;
 }

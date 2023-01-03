3d8e06a0750c43345ef80102e5559a80e3e9138c
Ifiok Ekott <ekottifiok@gmail.com>
2023-01-03 02:23:33 +0100

    fixed betty issues and removed redundant main block

--- 0x1E-search_algorithms/search_algos.h
+++ 0x1E-search_algorithms/search_algos.h
@@ -4,11 +4,9 @@
 /* NECESSARY IMPORTS */
 #include <stdio.h>
 #include <stdlib.h>
-#include <errno.h>
-#include <fenv.h>
- 
-#pragma STDC FENV_ACCESS ON
-/* FUNCTION DECLARATIONS */
+#include <math.h>
+
+ /* FUNCTION DECLARATIONS */
 int linear_search(int *array, size_t size, int value);
 int binary_search(int *array, size_t size, int value);
 int jump_search(int *array, size_t size, int value);

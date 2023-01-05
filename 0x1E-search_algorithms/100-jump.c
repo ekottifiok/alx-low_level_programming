1b26a3c14ba554267e017f4b9ee86ef0bd44236b
Ifiok Ekott <ekottifiok@gmail.com>
2023-01-05 05:59:05 +0100

    started working on f

--- 0x1E-search_algorithms/100-jump.c
+++ 0x1E-search_algorithms/100-jump.c
@@ -1,37 +1,42 @@
 #include "search_algos.h"
-#include <math.h>
 
 /**
- * jump_search - searches for a value in a sorted array
- * of integers using the Jump search algorithm
+ * jump_search - Search a value with a jump search algorithm
+ * @array: Array to search.
+ * @size: Size of the array to search.
+ * @value: Value to search.
  *
- * @array: pointer to the first element of the array to search in
- * @size: number of elements in array
- * @value: value to search for
- * Return: int
+ * Return: The index or -1 if it's not found
  */
+
 int jump_search(int *array, size_t size, int value)
 {
-	int diff, iter, jump_linear = 0, jump_step;
-	/* if jump_linear is 0 then jump algo is used and when 1 is linear algo */
+	int i, step = sqrt(size), j, flag = 0;
 
-	jump_step = sqrt((double)size);
-	for (iter = 0; iter < (int)size;)
+	for (i = 0; i < (int)size; i += step)
 	{
-		printf("Value checked array[%d] = [%d]\n", iter, array[iter]);
-		if (array[iter] >= value && jump_linear == 0)
+		if (array[i] >= value)
 		{
-			diff = iter - jump_step;
-			printf("Value found between indexes [%d] and [%d]\n", diff, iter);
-			iter -= jump_step;
-			jump_linear = 1;
+			printf("Value found between indexes [%d] and [%d]\n", i - step, i);
+			for (j = i - step; j <= i; j++)
+			{
+				printf("Value checked array[%d] = [%d]\n", j, array[j]);
+				if (array[j] == value)
+					return (j);
+			}
 		}
-		if (value == array[iter])
-			return (iter);
-		else if (jump_linear)
-			iter++;
-		else if (jump_linear == 0)
-			iter += jump_step;
+		printf("Value checked array[%d] = [%d]\n", i, array[i]);
+		if (i + step >= (int)size && flag == 0)
+		{
+			flag = 1;
+			printf("Value found between indexes [%d] and [%d]\n", i, i + step);
+			for (j = i; j < (int)size; j++)
+			{
+				printf("Value checked array[%d] = [%d]\n", j, array[j]);
+				if (array[j] == value)
+					return (j);
+			}
+		}
 	}
 	return (-1);
 }
---
+++ 0x1E-search_algorithms/106-linear_skip.c
@@ -0,0 +1,13 @@
+#include "search_algos.h"
+
+/**
+ * linear_skip -  searches for a value in a sorted skip list of integers
+ *
+ * @list: pointer to the head of the skip list to search in
+ * @value: value to search for
+ * Return: skiplist_t*
+ */
+skiplist_t *linear_skip(skiplist_t *list, int value)
+{
+    
+}

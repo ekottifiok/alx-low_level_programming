0bbcc59a21bd596147db653eebeb70246ccc619e
Ifiok Ekott <ekottifiok@gmail.com>
2023-01-05 05:16:03 +0100

    fixed all betty issues

--- 0x1E-search_algorithms/105-jump_list.c
+++ 0x1E-search_algorithms/105-jump_list.c
@@ -2,8 +2,10 @@
 
 /**
  * print_array - Print the array to search.
- * @start: First element to the subarray.
- * @end: Index of the end of the subarray.
+ * @index: index of the value position
+ * @prev_index: index of the previous value position
+ * @value: the value to be printed
+ * @mode: there are two modes to be printed
  *
  * Return: void.
  */
@@ -18,7 +20,8 @@ void print_array(int index, int prev_index, int value, int mode)
 }
 
 /**
- * sub_jump_list - value in a sorted list of integers using the Jump search algorithm.
+ * sub_jump_list - value in a sorted list of integers
+ * using the Jump search algorithm.
  *
  * @list: pointer to the head of the list to search in
  * @prev_list: pointer to the previous list
@@ -26,7 +29,7 @@ void print_array(int index, int prev_index, int value, int mode)
  * @value: Value to search.
  * @step: the step to jump
  * @flag_next_buffer: flags to ensure correct printing
- * @return listint_t*
+ * Return: listint_t*
  */
 listint_t *sub_jump_list(listint_t *list, listint_t *prev_list, size_t size,
 int value, int step, int flag_next_buffer)
@@ -36,7 +39,7 @@ int value, int step, int flag_next_buffer)
 
 	if (prev_list)
 		print_array(buffer->index, 0, buffer->n, 1);
-	
+
 	if (buffer->n == value && step == 1)
 	{
 		return (buffer);
@@ -44,21 +47,24 @@ int value, int step, int flag_next_buffer)
 	else if (buffer->n < value && buffer->next)
 	{
 		/* computes the next location */
-		for (iter = 0, next_buffer = buffer; iter < step && next_buffer->next; iter++)
+		for (iter = 0, next_buffer = buffer; iter < step && next_buffer->next;
+		iter++)
 			next_buffer = next_buffer->next;
 		if (!next_buffer->next && flag_next_buffer)
 		{
 			print_array(next_buffer->index, 0, next_buffer->n, 1);
 			print_array(next_buffer->index, buffer->index, 0, 0);
 			flag_next_buffer = 0;
-			return sub_jump_list(buffer, buffer, size, value, 1, flag_next_buffer);
-		}			
-		return sub_jump_list(next_buffer, buffer, size-1, value, step, flag_next_buffer);
+			return (sub_jump_list(buffer, buffer, size, value, 1, flag_next_buffer));
+		}
+		return (sub_jump_list(next_buffer, buffer, size - 1,
+		value, step, flag_next_buffer));
 	}
 	else if (buffer->n >= value)
 	{
 		print_array(buffer->index, prev_list->index, 0, 0);
-		return sub_jump_list(prev_list, prev_list, size, value, 1, flag_next_buffer);
+		return (sub_jump_list(prev_list, prev_list, size, value, 1,
+		flag_next_buffer));
 	}
 	return (NULL);
 }
